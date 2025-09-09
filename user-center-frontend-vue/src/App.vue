69c1b166546594bcc4b24b3903640349b5480120
YUKUN188 <dengchaokun1@outlook.com>
2025-09-09 16:52:27 +0800

    Add files via upload

---
+++ user-center-frontend-vue/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <BasicLayout />
+  </div>
+</template>
+
+<script setup lang="ts">
+import BasicLayout from "@/layouts/BasicLayout.vue";
+import { useLoginUserStore } from "@/store/useLoginUserStore";
+const loginUserStore = useLoginUserStore();
+loginUserStore.fetchLoginUser();
+</script>
+<style scoped>
+#basicLayout {
+}
+</style>
